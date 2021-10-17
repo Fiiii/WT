b1140ed5e71b73627c7db60442b658079d37c050
Artur Figiel <arturffigiel@gmail.com>
2021-10-17 19:01:45 +0200

    Flat HTTP connector with 'channeling' errors

--- app/main.go
+++ app/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
-func main()  {
-	fmt.Println("Hi WT")
+func main() {
+	if err := run(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	// Construct a server to service the requests against the mux.
+	httpServer := http.Server{
+		Addr: "localhost:3000",
+	}
+
+	// Channel for listening errors from http server
+	serverErrors := make(chan error, 1)
+
+	// Starting the server
+	go func() {
+		serverErrors <- httpServer.ListenAndServe()
+	}()
+
+	// =========================================================================
+	// Shutdown
+
+	// Blocking main and waiting for shutdown.
+	if err := <-serverErrors; err != nil {
+		return fmt.Errorf("server error: %w", err)
+	}
+
+	return nil
 }
