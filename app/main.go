284623d0bb9647c5dfda17e7c30eeb5c6b7498c6
Artur Figiel <arturffigiel@gmail.com>
2021-10-21 23:35:48 +0200

    Implement logger by zap & ardan config package

--- app/main.go
+++ app/main.go
@@ -3,24 +3,88 @@ package main
 import (
 	"fmt"
 	"github.com/Fiiii/WT/app/services/handlers"
+	"github.com/Fiiii/WT/foundation/logger"
+	"github.com/ardanlabs/conf/v2"
+	"go.uber.org/automaxprocs/maxprocs"
+	"go.uber.org/zap"
 	"net/http"
 	"os"
+	"runtime"
+	"time"
 )
 
 func main() {
-	if err := run(); err != nil {
+
+	// Construct the application logger.
+	log, err := logger.New("WT-API")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer log.Sync()
+
+	if err := run(log); err != nil {
 		os.Exit(1)
 	}
 }
 
-func run() error {
+func run(log *zap.SugaredLogger) error {
+
+	// =========================================================================
+	// GOMAXPROCS
+
+	// Set the correct number of threads for the service
+	// based on what is available either by the machine or quotas.
+	if _, err := maxprocs.Set(); err != nil {
+		return fmt.Errorf("maxprocs: %w", err)
+	}
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	// =========================================================================
+	// Configuration
+
+	cfg := struct {
+		conf.Version
+		Web struct {
+			APIHost         string        `conf:"default:0.0.0.0:3000"`
+			DebugHost       string        `conf:"default:0.0.0.0:4000"`
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:10s"`
+			IdleTimeout     time.Duration `conf:"default:120s"`
+			ShutdownTimeout time.Duration `conf:"default:20s"`
+		}
+	}{
+		Version: conf.Version{
+			Build: "dev",
+			Desc:  "",
+		},
+	}
 
 	// Api-calls mux
 	apiMux := handlers.APIMux()
 
+	// =========================================================================
+	// Start Debug Service
+
+	log.Infow("startup", "status", "debug router started", "host", cfg.Web.DebugHost)
+
+	// The Debug function returns a mux to listen and serve on for all the debug
+	// related endpoints. This include the standard library endpoints.
+
+	// Construct the mux for the debug calls.
+	//debugMux := handlers.DebugMux(build, log, db)
+
+	// Start the service listening for debug requests.
+	// Not concerned with shutting this down with load shedding.
+	//go func() {
+	//	if err := http.ListenAndServe(cfg.Web.DebugHost, debugMux); err != nil {
+	//		log.Errorw("shutdown", "status", "debug router closed", "host", cfg.Web.DebugHost, "ERROR", err)
+	//	}
+	//}()
+
 	// Construct a server to service the requests against the mux.
 	httpServer := http.Server{
-		Addr: "localhost:3000",
+		Addr:    "localhost:3000",
 		Handler: apiMux,
 	}
 
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/Fiiii/WT
 
 go 1.17
+
+require (
+	github.com/ardanlabs/conf/v2 v2.1.1 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/automaxprocs v1.4.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	go.uber.org/zap v1.19.1 // indirect
+)
